e58b8d0582c932e7e49ef9d01b8af8fac40c5c91
tom_pro <865124492@qq.com>
2019-06-20 22:11:32 +0800

    测试修改提交

--- src/main/java/com/mc/solrjdemo/solrjdemo/controller/TestController.java
+++ src/main/java/com/mc/solrjdemo/solrjdemo/controller/TestController.java
@@ -2,6 +2,6 @@ package com.mc.solrjdemo.solrjdemo.controller;
 
 public class TestController {
     public static void main(String[] args) {
-        System.out.println("哈哈哈风好大");
+        System.out.println("哈哈哈风好buda大");
     }
 }
