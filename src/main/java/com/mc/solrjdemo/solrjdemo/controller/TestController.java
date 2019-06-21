b0ede156edf215fdc8ab58afe1c064c4566f93e7
mc_company <865124493@qq.com>
2019-06-21 09:36:12 +0800

    测试公司idea提交

--- src/main/java/com/mc/solrjdemo/solrjdemo/controller/TestController.java
+++ src/main/java/com/mc/solrjdemo/solrjdemo/controller/TestController.java
@@ -3,5 +3,6 @@ package com.mc.solrjdemo.solrjdemo.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("哈哈哈风好buda大");
+        System.out.println("测试公司环境提交");
     }
 }
